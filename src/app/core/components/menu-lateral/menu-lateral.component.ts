6561a9dbb3e5938f46873fe9bdb69eaaf1a719ac
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-06-06 10:49:12 -0300

    feat(styles): move estilos scss para o arquivo base do projeto

---
+++ src/app/core/components/menu-lateral/menu-lateral.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-lateral',
+  templateUrl: './menu-lateral.component.html',
+  styleUrls: ['./menu-lateral.component.scss']
+})
+export class MenuLateralComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleMenu(): void {
+    const menu = document.getElementById('sidemenu');
+    const button = document.getElementsByClassName('togglemenu');
+    console.log(button);
+
+    if (button[0].classList.contains('open')) {
+      button[0].classList.remove('open');
+      button[0].classList.add('closed');
+      menu.style.marginLeft = '-7%';
+    } else if (button[0].classList.contains('closed')) {
+      button[0].classList.remove('closed');
+      button[0].classList.add('open');
+      menu.style.marginLeft = '0';
+    }
+  }
+}
