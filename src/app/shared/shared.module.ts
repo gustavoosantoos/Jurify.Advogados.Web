d5043a13259196d911cf16af6234f6403df67954
PatrickO98 <patrick.oliveira98@live.com>
2019-07-17 19:49:48 -0300

    Add jurify icon and logo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthenticationService } from './services/authentication.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
     declarations: [],
     imports: [
-      CommonModule
+      CommonModule,
+      MatFormFieldModule
     ],
     exports: [
       AuthenticationService
