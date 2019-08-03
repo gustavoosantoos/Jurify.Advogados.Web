b27c6a39bbc37f452fed1e227ad93a0e808b49a8
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-08-03 16:48:55 -0300

    add prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  authentication: {
+    provider: 'http://jurify-autenticador.azurewebsites.net/',
+    client_id: 'jurify.web.lawyers',
+    client_secret: 'jurify.web.lawyers',
+    scope: 'jurify.api.lawyers',
+    grant_type: 'password'
+  },
+  storage: {
+    token_identifier: 'jurify.web.lawyers.key',
+    user_info_identifier: 'jurify.web.lawyers.user'
+  },
+  endpoints: {
+    autenticador: 'http://jurify-autenticador.azurewebsites.net/api/',
+    advogados: 'http://jurify-advogados.azurewebsites.net/api/'
+  }
 };
