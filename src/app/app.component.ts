adf4ce507614219ce781ad6f3eaaf287f4f053ec
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-08-03 17:16:29 -0300

    fix route change observable on sidebar menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router'
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jurify-web';
-  router: string;
-
-  constructor(private _router: Router) {
-    this.router = _router.url;
+  currentRoute: string;
+  
+  constructor(private router: Router) { }
+  
+  ngOnInit(): void {
+    this.router.events.subscribe(e => {
+      if (e instanceof NavigationStart) {
+        this.currentRoute = e.url;
+      }
+    });
   }
 }
