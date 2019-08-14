11d38128e3b17f59a0f4939e89168004825c25e4
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-08-13 22:14:55 -0300

    feat(clientes): implementação do cadastro de clientes

--- src/app/modules/clientes/components/cadastro/cadastro.component.ts
+++ src/app/modules/clientes/components/cadastro/cadastro.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, TemplateRef } from '@angular/core';
 import { Location } from '@angular/common';
 import ClienteCadastro from '../../model/cadastro/cliente-cadastro.model';
 import EnderecoCadastro from '../../model/cadastro/endereco-cadastro.model';
+import ClientesService from '../../services/clientes.service';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
 
 @Component({
   selector: 'app-cadastro',
@@ -10,9 +12,33 @@ import EnderecoCadastro from '../../model/cadastro/endereco-cadastro.model';
 })
 export class CadastroComponent {
 
+  @ViewChild('errorsTemplate', { static: false })
+  errorTemplate: TemplateRef<any>;
+  
   cliente: ClienteCadastro = new ClienteCadastro();
+  validationErrors: string[] = [];
 
-  constructor(private location: Location) {
+  constructor(
+    private location: Location,
+    private clientesService: ClientesService,
+    private snackBar: MatSnackBar) {
+  }
+
+  salvarCliente(): void {
+    const snackBarOptions = new MatSnackBarConfig();
+    snackBarOptions.duration = 100000;
+
+    this.clientesService.cadastrarCliente(this.cliente).subscribe(r => {
+      console.log(r);
+    }, error => {
+      if (error.status == 400) {
+        console.log(error);
+        this.validationErrors = error.error.map(e => e.message);
+        this.snackBar.openFromTemplate(this.errorTemplate, snackBarOptions);
+      } else {
+        this.snackBar.open('Erro inesperado', 'Fechar', snackBarOptions);
+      }
+    });
   }
 
   goBack(): void {
