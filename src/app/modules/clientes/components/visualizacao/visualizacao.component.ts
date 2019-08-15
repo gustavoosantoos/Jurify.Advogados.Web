33af483bb2701b415a37eb34a48e4834c109b459
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-08-15 19:17:27 -0300

    feat(clientes): esqueleto da tela de visualização de clientes

--- src/app/modules/clientes/components/visualizacao/visualizacao.component.ts
+++ src/app/modules/clientes/components/visualizacao/visualizacao.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientesService } from '../../services/clientes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import Cliente from '../../model/visualizacao/cliente';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
 
 @Component({
   selector: 'app-visualizacao',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualizacao.component.scss']
 })
 export class VisualizacaoComponent implements OnInit {
+  public cliente: Cliente = new Cliente();
 
-  constructor() { }
+  constructor(
+    private clientesService: ClientesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit() {
-  }
+    this.activatedRoute.paramMap.subscribe(r => {
+      const snackBarConfig = new MatSnackBarConfig();
+      snackBarConfig.duration = 10000;
 
+      const codigoCliente = r.get('codigo');
+      this.clientesService.getCliente(codigoCliente).subscribe(c => {
+        this.cliente = c;
+      }, error => {
+        this.snackBar.open('Erro ao carregar cliente', 'Fechar', snackBarConfig);
+        this.router.navigateByUrl('/clientes');
+      });
+    });
+  }
 }
