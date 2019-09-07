db80ed50fab422e3b0d8c346c23eca34645ce117
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-09-07 18:01:14 -0300

    fix(processos-juridicos): adicionar import do componente de cadastros no módulo

--- src/app/modules/processos/processos.module.ts
+++ src/app/modules/processos/processos.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 import { ProcessosRoutingModule } from './processos-routing.module';
 import { ListagemComponent } from './components/listagem/listagem.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @NgModule({
-  declarations: [ListagemComponent],
+  declarations: [
+    ListagemComponent,
+    CadastroComponent
+  ],
   imports: [
     SharedModule,
     ProcessosRoutingModule
