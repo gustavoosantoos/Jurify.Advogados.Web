da38350970a43383b5afbb25ebf73d1dc4882d61
dlqseronato <dlqseronato@gmail.com>
2019-10-10 19:10:18 -0300

    Corrigindo routing

--- src/app/shared/services/signup.service.ts
+++ src/app/shared/services/signup.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SignUp } from '../model/signup';
 import Cadastro from 'src/app/modules/usuarios/model/cadastro.model';
