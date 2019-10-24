dbd6e52e2398d9e810a8be70edce002dc62b62b5
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-10-23 23:59:10 -0300

    ajusta para https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,6 +14,6 @@ export const environment = {
   },
   endpoints: {
     autenticador: 'https://jurify-autenticador.azurewebsites.net/api/',
-    advogados: 'http://jurify-advogados.azurewebsites.net/api/'
+    advogados: 'https://jurify-advogados.azurewebsites.net/api/'
   }
 };
